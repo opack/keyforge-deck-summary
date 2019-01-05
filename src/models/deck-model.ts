3deffa99fc753e3a6559c09c222ea3c976db6626
opack <github@valdera.fr>
2019-01-05 21:18:21 +0100

    Fix hashed deck not hashing cards content, and now using sha1 library, way more faster!

--- src/models/deck-model.ts
+++ src/models/deck-model.ts
@@ -12,13 +12,6 @@ export class DeckModel {
   cards: Array<CardModel>;
 
   constructor(nbCards = NB_CARDS) {
-    // // We must define initial values so that the hash computed on the deck can be compared. Indeed, when
-    // // the UI is initialized, all these values receive "undefined". However, when the deck was initially
-    // // created, the fields did not exist (unless we initialized them) and thus the hash is different.
-    // this.name = '';
-    // this.qrcode = '';
-    // this.backgroundImage = '';
-
     this.cards = new Array<CardModel>(nbCards);
     for (let cur = 0; cur < nbCards; cur++) {
       this.cards[cur] = new CardModel();
